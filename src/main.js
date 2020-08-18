11edb1d14b37a5a68fb64607723524501b3b8b64
Suizhazha <573505403@qq.com>
2020-08-18 15:52:02 +0800

    自定义directives,模拟v-on2操作DOM

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
-import Vue from "vue/dist/vue.js";
+import Vue from "vue/dist/vue.js"
 
 Vue.config.productionTip = false
 
 new Vue({
-
-  template:`
-  <div>
-  <button v-on2:click = 'hi'>点我</button>
-</div>
+//自定义指令v-on2
+  directives: {
+    "on2": {
+      //bind,inserted都可以
+      bind(el, info) {
+        console.log(info)
+        //通过查看info对象，查找到arg对应的click事件和value对应的hi函数
+        el.addEventListener(info.arg, info.value)
+      },
+      //元素消亡后，回收事件监听
+      unbind(el, info) {
+        el.removeEventListener(info.arg, info.value)
+      }
+    }
+  },
+  template: `
+    <button v-on2:click='hi'>点我</button>
   `,
-  methods:{
-    hi(){
-      console.log('hi')
+  methods: {
+
+    hi() {
+      console.log("hi")
     }
   }
-}).$mount('#app')
+}).$mount("#app")
